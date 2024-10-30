08ba76806c9463605c250b26fad36946af39f024
andyzhuzh <117624271+andyzhuzh@users.noreply.github.com>
2024-10-30 14:26:46 +0800

    Update config.go

--- common/config/config.go
+++ common/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Config struct {
-	viperObj   *viper.Viper
+	ViperObj   *viper.Viper
 	FileName   string
 	ConfigType string
 }
@@ -53,12 +53,12 @@ func GetConfig(conFileName string) (conf Config, err error) {
 		err = viperTMP.ReadInConfig()
 	}
 
-	conf.viperObj = viperTMP
+	conf.ViperObj = viperTMP
 	return
 
 }
 func (config *Config) SetValue(key string, value any) {
-	config.viperObj.Set(key, value)
+	config.ViperObj.Set(key, value)
 	// viper.Set("host.port", 5899)
 	// viper.SetDefault("ContentDir", "content")
 	// viper.SetDefault("LayoutDir", "layouts")
@@ -66,7 +66,7 @@ func (config *Config) SetValue(key string, value any) {
 }
 
 func (config *Config) GetValue(key string) any {
-	return config.viperObj.Get(key)
+	return config.ViperObj.Get(key)
 	// GetFloat64(key string) : float64
 	// GetIntSlice(key string) : []int
 	// GetStringMap(key string) : map[string]any
@@ -79,21 +79,21 @@ func (config *Config) GetValue(key string) any {
 }
 
 func (config *Config) GetValueString(key string) string {
-	return config.viperObj.GetString(key)
+	return config.ViperObj.GetString(key)
 }
 func (config *Config) GetValueBool(key string) bool {
-	return config.viperObj.GetBool(key)
+	return config.ViperObj.GetBool(key)
 }
 func (config *Config) GetValueInt(key string) int {
-	return config.viperObj.GetInt(key)
+	return config.ViperObj.GetInt(key)
 }
 
 func (config *Config) GetSub(key string) *viper.Viper {
-	return config.viperObj.Sub(key)
+	return config.ViperObj.Sub(key)
 }
 
 func (config *Config) SaveConfig() (err error) {
-	err = config.viperObj.WriteConfig()
+	err = config.ViperObj.WriteConfig()
 	return
 }
 
@@ -105,6 +105,6 @@ func (config *Config) Unmarshal(rawVal any, opts ...viper.DecoderConfigOption) (
 	// }
 	// var C config
 	// v.Unmarshal(&C)
-	err = config.viperObj.Unmarshal(rawVal, opts...)
+	err = config.ViperObj.Unmarshal(rawVal, opts...)
 	return
 }
